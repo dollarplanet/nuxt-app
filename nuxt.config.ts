40d30b9646b336f3ca663d435e934000ea62a1a7
dollarplanet <agusriyanto.artist@gmail.com>
2025-12-29 15:33:06 +0700

    sample nuxt with content studio

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/content", "@nuxt/eslint", "nuxt-studio"],
+  studio: {
+    repository: {
+      provider: "github",
+      owner: "dollarplanet",
+      repo: "nuxt-app",
+      branch: "master",
+    },
+    auth: {
+      google: {},
+    },
+  },
+});
